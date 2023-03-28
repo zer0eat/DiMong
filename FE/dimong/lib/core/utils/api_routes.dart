372aca12f53c0a6881dc6f9787c3643817637332
김소현 <alice2596@naver.com>
2023-03-28 12:19:12 +0900

    fix(main.dart): page routing 수정
    main.dart: page routing 수정
    api_routes.dart: api endpoint 오타수정

--- FE/dimong/lib/core/utils/api_routes.dart
+++ FE/dimong/lib/core/utils/api_routes.dart
@@ -15,6 +15,6 @@ class Paths {
   static const String themeList = '/api/v1/dinosaurs'; // 주제맞춰그리기 리스트
   static const String drawingDetail = '/api/v1/dinosaurs/{dinosaurId}/characteristic'; // 주제맞춰그리기 특징
   static const String badgeCheck = '/api/v1/drawings/guide'; // 뱃지확인
-  static const String cameraDino = '/api/ai/b1/pictures/dinosaurs'; // 카메라
+  static const String cameraDino = '/api/ai/v1/pictures/dinosaurs'; // 카메라
 
 }
