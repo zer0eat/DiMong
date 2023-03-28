b61b424e1c322defceedbeae49111cf40bc5c957
김영식 <kimyoungsik7@naver.com>
2023-03-28 15:02:30 +0900

    feat: 공룡 이진분류모델 완성
    공룡 이미지 입력 시 이진분류 모델로 판단 후 공룡일 경우에만 분류
    
    closes #S08P22A105-135, #S08P22A105-201

--- AI/AI_API/ai/PictureAI.py
+++ AI/AI_API/ai/PictureAI.py
@@ -12,8 +12,12 @@ from models.PictureModel import Picture
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # dimong 분석모델 load
+model_or_not = torch.load("C:\\Users\\SSAFY\\Desktop\\fastapi_test\\ai\\models\\dimong_model_or_not.pth", map_location=torch.device('cpu'))
 model = torch.load("C:\\Users\\SSAFY\\Desktop\\fastapi_test\\ai\\models\\dimong_model.pth", map_location=torch.device('cpu'))
+
+model_or_not.eval()
 model.eval()
+dino_or_not = ["dino", -1]
 dinosaur_names = ['Allosaurus', 'Ankylosaurus', 'Argentinosaurus', 'Brachiosaurus', 'Chasmosaurus', 'Compsognathus', 'Dacentrurus', 'Dilophosaurus', 'Dimorphodon', 'Elasmosaurus', 'Giganotosaurus', 'Iguanodon', 'Incisibosaurus', 'Kentrosaurus', 'Lambeosaurus', 'Mosasaurus', 'Nodosaurus', 'Ouranosaurus', 'Oviraptor', 'Pachycephalosaurus', 'Parasaurolophus', 'Pteranodon', 'Quetzalcoatlus', 'Sauropaganax', 'Spinosaurus', 'Stegosaurus', 'Triceratops', 'Tsintaosaurus', 'Tyrannosaurus', 'Velociraptor']
 
 # 이미지파일 전처리
@@ -25,9 +29,6 @@ transformations = transforms.Compose([
     # 이미지의 RGB 채널 값을 평균과 표준편차를 이용하여 정규화
     transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
-
-
-
 
 class PictureAI:
     def __init__(
@@ -45,11 +46,21 @@ class PictureAI:
         # 모델의 파라미터가 업데이트 되지 않고 연산의 중복을 막아 빠른 결과를 출력
         with torch.no_grad():
             # model에 image를 넣어 예측
-            outputs = model(image)
+            outputs = model_or_not(image)
             # torch.max 함수를 이용해 출력값 중 가장 큰 값을 가지는 인덱스
             _, preds = torch.max(outputs, 1)
-        # 예측한 결과 preds에서 가장 확률이 높은 클래스를 dinosaur_names 리스트에서 찾아 반환
-        return dinosaur_names[preds[0]]
+
+        if dino_or_not[preds[0]] == -1:
+            return -1
+        else:
+            # 모델의 파라미터가 업데이트 되지 않고 연산의 중복을 막아 빠른 결과를 출력
+            with torch.no_grad():
+                # model에 image를 넣어 예측
+                outputs = model(image)
+                # torch.max 함수를 이용해 출력값 중 가장 큰 값을 가지는 인덱스
+                _, preds = torch.max(outputs, 1)
+            # 예측한 결과 preds에서 가장 확률이 높은 클래스를 dinosaur_names 리스트에서 찾아 반환
+            return dinosaur_names[preds[0]]
 
     def exec(self, image: bytes) -> Picture:
         picture: Picture = Picture()
