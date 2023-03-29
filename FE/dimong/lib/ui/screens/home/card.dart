4a90adb99cac5ae2ac27bf7391c6dbccc318db3f
lkjin1202 <lkjin1202@gmail.com>
2023-03-30 00:46:09 +0900

    desing: home_page.dart 리리빌딩

--- FE/dimong/lib/ui/screens/home/card.dart
+++ FE/dimong/lib/ui/screens/home/card.dart
@@ -1,28 +1,28 @@
 import 'package:flutter/material.dart';
 
 class CurrencyCard extends StatelessWidget {
-  final String name, subText, dino;
+  final String name, dino;
   final IconData icon;
-  final bool isInverted, xDirection;
+  final bool isInverted;
 
   const CurrencyCard({
     super.key,
     required this.name,
-    required this.subText,
     required this.icon,
     required this.isInverted,
-    required this.xDirection,
     required this.dino,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+      padding: const EdgeInsets.all(2),
+      height: 180,
+      margin: const EdgeInsets.all(5),
       clipBehavior: Clip.hardEdge,
       decoration: BoxDecoration(
         color: isInverted ? Colors.white : const Color(0xFFACC864),
-        borderRadius: BorderRadius.circular(20),
+        borderRadius: BorderRadius.circular(10),
         boxShadow: [
           BoxShadow(
             color: Colors.grey.withOpacity(0.3),
@@ -33,35 +33,28 @@ class CurrencyCard extends StatelessWidget {
         ],
       ),
       child: Padding(
-        padding: const EdgeInsets.all(30),
-        child: Row(
-          mainAxisAlignment: xDirection
-              ? MainAxisAlignment.spaceBetween
-              : MainAxisAlignment.start,
+        padding: const EdgeInsets.all(7),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Container(
-              alignment: const AlignmentDirectional(-100, -100),
-              child: Image(
-                image: AssetImage(dino),
-                width: 100,
-                alignment: const AlignmentDirectional(-100, -100),
-              ),
+            Image(
+              image: AssetImage(dino),
+              width: 80,
+              height: 100,
             ),
             Column(
-              crossAxisAlignment: xDirection
-                  ? CrossAxisAlignment.end
-                  : CrossAxisAlignment.start,
               children: [
                 // 메뉴 아이콘
                 Icon(icon,
-                    shadows: const <Shadow>[
-                      Shadow(
-                        color: Colors.grey,
-                        blurRadius: 5.0,
-                      )
-                    ],
-                    color: isInverted ? const Color(0xFFACC864) : Colors.white,
-                    size: 30),
+                  shadows: const <Shadow>[
+                    Shadow(
+                      color: Colors.grey,
+                      blurRadius: 5.0,
+                    )
+                  ],
+                  color: isInverted ? const Color(0xFFACC864) : Colors.white,
+                  size: 30),
 
                 // 메뉴 이름
                 Text(
@@ -72,18 +65,6 @@ class CurrencyCard extends StatelessWidget {
                     fontWeight: FontWeight.w800,
                   ),
                 ),
-
-                Row(
-                  children: [
-                    Text(
-                      subText,
-                      style: const TextStyle(
-                        color: Color(0xFF6B6B6B),
-                        fontSize: 12,
-                      ),
-                    ),
-                  ],
-                )
               ],
             ),
           ],
