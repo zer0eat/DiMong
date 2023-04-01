6e7ccd0e2130b7072f1acc72c3748162e831684a
s5hyeonkim <zxas2596@gmail.com>
2023-04-01 11:28:21 +0900

    fix(user_repository.dart): 로그인 변경 시, 코드 수정
    user_repository.dart: 로그인 변경 시, user_profile 접근 코드 수정
    data.dart: 로그인 변경 시, 통신 코드 수정

---
+++ FE/dimong/lib/core/login/user_repository.dart
@@ -0,0 +1,14 @@
+import './user_profile.dart';
+import './store_user_repository.dart';
+
+abstract class UserRepository {
+  final SharedPreferencesUserRepository _sharedPreferencesUserRepository = SharedPreferencesUserRepository();
+
+  Future<void> saveUserProfile(UserProfile userProfile) async {
+    await _sharedPreferencesUserRepository.saveUserProfile(userProfile);
+  }
+
+  Future<UserProfile?> getUserProfile() async {
+    return await _sharedPreferencesUserRepository.getUserProfile();
+  }
+}
---
+++ FE/dimong/lib/core/login/user_service.dart
@@ -0,0 +1,17 @@
+import './user_profile.dart';
+import './user_repository.dart';
+
+class UserService {
+  final UserRepository _userRepository;
+
+  UserService({required UserRepository userRepository})
+      : _userRepository = userRepository;
+
+  Future<void> saveUserProfile(UserProfile userProfile) async {
+    await _userRepository.saveUserProfile(userProfile);
+  }
+
+  Future<UserProfile?> getUserProfile() async {
+    return await _userRepository.getUserProfile();
+  }
+}
