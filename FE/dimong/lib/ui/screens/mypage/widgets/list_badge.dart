496fe8796651b1a11cf538236d44158be6ddff95
양희진 <syhj0326@naver.com>
2023-04-06 08:43:08 +0900

    ëfix: 뱃지 오류 수정

--- FE/dimong/lib/ui/screens/mypage/widgets/list_badge.dart
+++ FE/dimong/lib/ui/screens/mypage/widgets/list_badge.dart
@@ -13,38 +13,43 @@ class MyCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(child: Card(
-      color: Colors.white.withOpacity(0.9 ),
-      elevation: 4,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(50),
-      ),
-      child: Container(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            if (collected == false)
-              Image.asset(
-                'assets/egg/${badgeName}.png',
-                width: double.infinity,
-                fit: BoxFit.contain,
+    return Row(
+      children: [
+        Expanded(
+          child: Card(
+            color: Colors.white.withOpacity(0.9),
+            elevation: 4,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(50),
+            ),
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  if (collected == false)
+                    Image.asset(
+                      'assets/egg/${badgeName}.png',
+                      width: double.infinity,
+                      fit: BoxFit.contain,
+                    ),
+                  if (collected == true)
+                    Tooltip(
+                      waitDuration: Duration(microseconds: 10),
+                      showDuration: Duration(seconds: 2),
+                      message: "이름: ${badgeName}\n생성일자: ${create}",
+                      child: Image.asset(
+                        'assets/badges/${badgeName}.png',
+                        width: double.infinity,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                ],
               ),
-            if (collected == true)
-              Tooltip(
-                waitDuration: Duration(microseconds: 10),
-                showDuration: Duration(seconds: 2),
-                message: "이름: ${badgeName}\n생성일자: ${create}",
-                child: Image.asset(
-                  'assets/badges/${badgeName}.png',
-                  width: double.infinity,
-                  fit: BoxFit.contain,
-                ),
-              ),
-
-          ],
+            ),
+          ),
         ),
-      ),
-    ),);
+      ],
+    );
   }
 }
